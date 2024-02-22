ed1836aee46c3824fcbcfd6bb6720497ba4e0b0b
Yyuno <yyuno@Yyunos-MBP.local>
2024-02-22 17:28:34 +0100

    ...

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -4,27 +4,35 @@ void ft_putchar(char c) {
     write(1, &c, 1);
 }
 
-void ft_print_comb2(void) {
-    int i, j;
-
-    for (i = 0; i <= 98; i++) {
-        for (j = i + 1; j <= 99; j++) {
-            ft_putchar(i / 10 + '0');
-            ft_putchar(i % 10 + '0');
-            ft_putchar(' ');
-            ft_putchar(j / 10 + '0');
-            ft_putchar(j % 10 + '0');
-
-            if (i != 98 || j != 99) {
-                ft_putchar(',');
-                ft_putchar(' ');
-            }
-        }
+void ft_print_comb2_recursive(int i, int j) {
+    if (i > 98) {
+        return;
     }
+
+    if (j > 99) {
+        ft_print_comb2_recursive(i + 1, i + 2);
+        return;
+    }
+
+    ft_putchar(i / 10 + '0');
+    ft_putchar(i % 10 + '0');
+    ft_putchar(' ');
+    ft_putchar(j / 10 + '0');
+    ft_putchar(j % 10 + '0');
+
+    if (i != 98 || j != 99) {
+        ft_putchar(',');
+        ft_putchar(' ');
+    }
+
+    ft_print_comb2_recursive(i, j + 1);
 }
-/*
+
+void ft_print_comb2(void) {
+    ft_print_comb2_recursive(0, 1);
+}
+
 int main(void) {
     ft_print_comb2();
     return 0;
 }
-*/
